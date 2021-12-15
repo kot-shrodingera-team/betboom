f5ae24d40b464cbb4629e194be73d93ae5179fad
Kot Shrodingera <kot.shrodingera@gmail.com>
2021-12-15 11:17:07 +0100

    fix(goToLive): разделльный алгоритм для betboom и pinup

--- src/helpers/goToLive.ts
+++ src/helpers/goToLive.ts
@@ -5,20 +5,32 @@ import {
 } from '@kot-shrodingera-team/germes-utils/errors';
 
 const goToLive = async (): Promise<void> => {
-  const liveLink = await getElement<HTMLElement>(
-    // betboom, pinup
-    '.js-gtm-click-menu-item--live > a, .nav__link[href="/live"]',
-    5000
-  );
-  if (!liveLink) {
-    throw new JsFailError('Не найдена кнопка перехода на Live');
-  }
+  if (window.location.hostname === 'pin-up.ru') {
+    const liveLink = await getElement<HTMLElement>(
+      '.nav__link[href="/live"]',
+      5000
+    );
+    if (!liveLink) {
+      throw new JsFailError('Не найдена кнопка перехода на Live');
+    }
 
-  if (liveLink.classList.contains('nav__link--active')) {
-    log('Уже открыт Live', 'cadetblue', true);
+    if (liveLink.classList.contains('nav__link--active')) {
+      log('Уже открыт Live', 'cadetblue', true);
+    } else {
+      liveLink.click();
+      throw new NewUrlError('Переходим на Live');
+    }
   } else {
-    liveLink.click();
-    throw new NewUrlError('Переходим на Live');
+    const liveLink = await getElement<HTMLElement>(
+      '.js-gtm-click-menu-item--live > a',
+      5000
+    );
+    if (window.location.pathname.startsWith('/sport')) {
+      log('Уже открыт Live', 'cadetblue', true);
+    } else {
+      liveLink.click();
+      throw new NewUrlError('Переходим на Live');
+    }
   }
 };
 
