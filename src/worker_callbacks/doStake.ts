f8213a8f7cece900638b9378d9ad6b90acaf3344
Kot Shrodingera <kot.shrodingera@gmail.com>
2021-12-16 14:01:40 +0100

    feat(doStake): проверка на изменения в купоне перед ставкой

--- src/worker_callbacks/doStake.ts
+++ src/worker_callbacks/doStake.ts
@@ -1,16 +1,23 @@
 import doStakeGenerator from '@kot-shrodingera-team/germes-generators/worker_callbacks/doStake';
+import { log } from '@kot-shrodingera-team/germes-utils';
 import getCoefficient from '../stake_info/getCoefficient';
 
-// const preCheck = (): boolean => {
-//   return true;
-// };
+const preCheck = (): boolean => {
+  const acceptButton = document.querySelector<HTMLElement>('.tg__accept');
+  if (acceptButton) {
+    log('Принимаем изменения. Ставку не делаем', 'orange');
+    acceptButton.click();
+    return false;
+  }
+  return true;
+};
 
 // const postCheck = (): boolean => {
 //   return true;
 // };
 
 const doStake = doStakeGenerator({
-  // preCheck,
+  preCheck,
   doStakeButtonSelector:
     '.favAmmButtons [value="ЗАКЛЮЧИТЬ ПАРИ"], .favAmmButtons [value="СДЕЛАТЬ СТАВКУ"], .favAmmButtons [value="Сделать ставку"]',
   // errorClasses: [
